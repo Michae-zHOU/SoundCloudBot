c860c33b8f920efb8a871dc57f7bcef87f952b92
Michae-zHOU <michael.zhouzzy@gmail.com>
2022-02-03 23:43:54 -0800

    Remove duplicated import.

--- sc_bot.py
+++ sc_bot.py
@@ -8,7 +8,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 class SoundCloudBot:
