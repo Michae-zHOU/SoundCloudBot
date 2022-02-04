691cfe0dabf84ba76e90e54c0f0218450fce631f
Michae-zHOU <michael.zhouzzy@gmail.com>
2022-02-03 17:41:35 -0800

    Fix number extraction helper method.

--- helper.py
+++ helper.py
@@ -1,4 +1,8 @@
 import re
 
 def get_number_from_str(s):
-    return int(re.findall('[0-9]+', s)[0])
+    emp_str = ""
+    for m in s:
+        if m.isdigit():
+            emp_str = emp_str + m
+    return int(emp_str)
