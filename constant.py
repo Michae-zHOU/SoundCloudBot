4cebc35d3a8cc61a9250165924619a6e2013599c
Michae-zHOU <michael.zhouzzy@gmail.com>
2022-02-03 23:41:01 -0800

    Move constant values to another file.

---
+++ constant.py
@@ -0,0 +1,10 @@
+DEFAULT_RUN = 1
+PLAY_TIME = 10
+PAUSE_TIME = 2
+LOADING_TIMEOUT = 20
+STABLE_THRESH_HOLD = 10
+DEFAULT_URL = 'https://soundcloud.com/decentral-mike/basshead-mix2'
+PLAY_BUTTON_XPATH = '//*[@id="content"]/div/div[2]/div/div[2]/div[2]/div/div/div[1]'
+VOLUME_BUTTON_XPATH = '//*[@id="app"]/div[4]/section/div/div[3]/div[5]/div/div[2]/button'
+PLAY_TAB = '//*[@id="content"]/div/div[3]/div[1]/div/div[1]/div/div/div[2]/ul/li[1]/span/span[1]'
+ENABLE_WAIT=False
